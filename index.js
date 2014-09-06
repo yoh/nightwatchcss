38658f9fdae3c3d9592be0fe0ebb9467edab8cc6
Johan Gaulupeau <yoh91@msn.com>
2014-09-07 01:28:09 +0200

    change WebdriverCss (WebdriverIO) to work with Nightwatch.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
  * WebdriverCSS
  * Regression testing tool for WebdriverJS
- * 
+ *
  * @author Christian Bromann <mail@christian-bromann.com>
  * @license Licensed under the MIT license.
  */
 
-module.exports = process.env.WEBDRIVERCSS_COVERAGE === '1' ? require('./lib-cov/webdrivercss.js') : require('./lib/webdrivercss.js');
+module.exports = process.env.WEBDRIVERCSS_COVERAGE === '1' ? require('./lib-cov/nightwatchcss.js') : require('./lib/nightwatchcss.js');
